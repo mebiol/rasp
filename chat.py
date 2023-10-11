b3c151e492c4e8159e118e1f0ec74bbd72b40502
mebiol <thunchanok1944@gmail.com>
2023-10-11 10:06:58 +0700

    add chat.py

---
+++ chat.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+
+# Initialize PyAudio and SpeechRecognition
+mic = sr.Microphone(1)
+recog = sr.Recognizer()
+lan='th'
+
+while True:
+    with mic as source:
+        print("Listening for speech...")
+        audio = recog.listen(source)
+    try:
+        text = recog.recognize_google(audio, language='th-TH')
+        print(text)
+        sound = gTTS(text=text, lang=lan, slow=False)
+        sound.save('test.mp3')
+        os.system('test.mp3')
+    except sr.WaitTimeoutError:
+        print("Recognition timed out")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
--- test_chat.py
+++ test_chat.py
@@ -3,46 +3,45 @@ from gtts import gTTS
 import os
 import time
 import openai
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-openai.api_key = os.getenv('PASSWORD')
-model = 'gpt-3.5-turbo'
+# load_dotenv()
+# openai.api_key = os.getenv('PASSWORD')
+# model = 'gpt-3.5-turbo'
 
-
+# print(sr.Microphone.list_microphone_names ())
 
 def transcribe_mic():
-        recog = sr.Recognizer()
-        mic = sr.Microphone(device_index=1)
-        lan='th'
-        while True:
-                print('Listening...')
-                with mic as source:
-#                       recog.adjust_for_ambient_noise(source,duration=2)
-#                       recog.energy_threshold = 4000
-#                       audio = recog.listen(source)
-                        audio = recog.listen(source,timeout=3,phrase_time_limit=15)
-                try:
-                        print("processing...")
-                        text = recog.recognize_google(audio,language='th-TH')
-                        response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                                messages = [{"role":"user",
-                                                                "content":f"{text}"}])
-                        res_txt = response.choices[0].messages.content
-                        print(res_txt)
-                        sound = gTTS(text=res_text,lang=lan,slow=False)
+    recog = sr.Recognizer()
+    mic = sr.Microphone(device_index=1)
+    lan = 'th'
+    while True:
+        print('Listening...')
+        try:
+            with mic as source:
+                audio = recog.listen(source, timeout=5)  # Set a timeout to prevent hanging
+                if audio is not None:
+                        print("Processing...")
+                        text = recog.recognize_google(audio, language='th-TH')
+                        print(text)
+                        sound = gTTS(text=text, lang=lan, slow=False)
                         sound.save('test.mp3')
-                        os.system('cvlc --play-and-exit test.mp3')
-                except sr.UnknownValueError:
-                        time.sleep(2)
-                        print('Silence found, shutting up...')
+                        os.system('test.mp3')
+                else:
                         continue
-                except sr.RequestError as e:
-                        print(f'Could not request result from google Speech Recognition service; {e}')
-                        continue
+        except sr.WaitTimeoutError:
+            print('No speech detected. Listening again...')
+            continue
+        except sr.UnknownValueError:
+            print('Speech recognition could not understand audio')
+        except sr.RequestError as e:
+            print(f'Could not request result from Google Speech Recognition service: {e}')
+        except Exception as e:
+            print(f'An error occurred: {e}')
 
 def main():
-        transcribe_mic()
+    transcribe_mic()
 
 if __name__ == "__main__":
-        main()
+    main()
+
