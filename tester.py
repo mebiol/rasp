97a1c74501a370d2b0c3086e91c0b7a6e86a12f1
mebiol <thunchanok1944@gmail.com>
2023-10-11 10:44:47 +0700

    handle assert

--- tester.py
+++ tester.py
@@ -8,25 +8,27 @@ recog = sr.Recognizer()
 lan = 'th'
 
 def transcribe_mic():
-    with mic as source:
-        print("Adjusting for ambient noise...")
-        recog.adjust_for_ambient_noise(source, duration=1)
-        print("Listening for speech...")
+    while True:
         try:
-            audio = recog.listen(source, timeout=3)
-            text = recog.recognize_google(audio, language='th-TH')
-            print(text)
-            sound = gTTS(text=text, lang=lan, slow=False)
-            sound.save('test.mp3')
-            os.system('cvlc --play-and-exit test.mp3')
+            with mic as source:
+                print("Adjusting for ambient noise...")
+                recog.adjust_for_ambient_noise(source, duration=1)
+                print("Listening for speech...")
+                audio = recog.listen(source, timeout=3)
+                text = recog.recognize_google(audio, language='th-TH')
+                print(text)
+                sound = gTTS(text=text, lang=lan, slow=False)
+                sound.save('test.mp3')
+                os.system('cvlc --play-and-exit test.mp3')
         except sr.WaitTimeoutError:
             print("Recognition timed out")
+        except AssertionError:
+            print("No audio source available. Waiting for audio source...")
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
 def main():
-    while True:
-        transcribe_mic()
+    transcribe_mic()
 
 if __name__ == "__main__":
     main()
