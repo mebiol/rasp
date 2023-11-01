86ac490724f8be85307c71a44f3c4b8297dbc0c3
mebiol <thunchanok1944@gmail.com>
2023-11-01 16:18:37 +0700

    fix 75%

--- tester.py
+++ tester.py
@@ -1,24 +1,37 @@
 import speech_recognition as sr
 from gtts import gTTS
 import requests
+import browser_cookie3
 from bardapi import Bard
 import os
 import re
+import time
 import IPython
-
 
 # Initialize PyAudio and SpeechRecognition
 mic = sr.Microphone(1)
 recog = sr.Recognizer()
 lan = 'th'
 
-res = requests.get("http://192.168.1.104:5001/api")
-data = res.json()
-msg = data['msg']
+cj = browser_cookie3.firefox()
+secure_1psid_cookie = None
+
+for cookie in cj:
+    if cookie.name == '__Secure-1PSID':
+        secure_1psid_cookie = cookie.value
+        break
+
 Apikey = 'd42uuQuLvWm13dAjiBmgFkdFPpsnPzvL'
 
 chosen_system = input("Choose TTS system (vaja/google): ")
 chosen_mode = None if chosen_system == "google" else input('Select mode in 0-4: ')
+
+def play_sound(chosen_system, name):
+    """Play the sound based on the chosen system and filename."""
+    if chosen_system == 'vaja':
+        os.system(f'cvlc --play-and-exit {name}.wav')
+    elif chosen_system == 'google':
+        os.system(f'cvlc --play-and-exit {name}.mp3')
 
 def listen_and_transcribe():
     with mic as source:
@@ -27,50 +40,54 @@ def listen_and_transcribe():
         print("Listening for speech...")
         audio = recog.listen(source, timeout=2)
         text = recog.recognize_google(audio, language='th-TH')
-        return text 
+        return text
 
-def synth_vaja(data, name):
+def synth_vaja(data, name, max_retries=5):
     url = 'https://api.aiforthai.in.th/vaja9/synth_audiovisual'
     headers = {'Apikey': Apikey, 'Content-Type': 'application/json'}
-    response = requests.post(url, json=data, headers=headers)
-    status = True
-    while(status):
+    
+    for i in range(max_retries):
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            response.raise_for_status()  # This will raise an HTTPError if the response was an error
+            
+            if 'wav_url' not in response.json():
+                print("Error: 'wav_url' not found in the response!")
+                return None
+            
+            status = True
+            while status:
+                resp = requests.get(response.json()['wav_url'], headers={'Apikey': Apikey})
+                if resp.status_code == 200:
+                    with open(f'{name}.wav', 'wb') as a:
+                        a.write(resp.content)
+                        print('Downloaded: ')
+                        IPython.display.display(IPython.display.Audio(f'{name}.wav'))
+                        status = False 
+                else:
+                    print(resp.reason)
+            return response.json() 
 
-        resp = requests.get(response.json()['wav_url'],headers={'Apikey':Apikey})
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 429 and i < max_retries - 1:
+                print("Rate limit exceeded. Retrying...")
+                time.sleep(2 ** i)  # Exponential backoff
+            else:
+                print(f"Error occurred: {e}")
+                return None
 
-        if resp.status_code == 200:
-            with open(f'{name}.wav', 'wb') as a:
-                a.write(resp.content)
-                print('Downloaded: ')
-                IPython.display.display(IPython.display.Audio(f'{name}.wav'))
-                #os.system(f'cvlc --play-and-exit {name}.wav')
-                status = False 
-   
-        else:
-                print(resp.reason)
-             
-    return response.json()    
- 
+
 def google_tts(text, lang, name):
     sound = gTTS(text=text, lang=lang, slow=False)
     sound.save(f'{name}.mp3')
-    #os.system(f'cvlc --play-and-exit {name}.mp3')
 
 def ask_tts_system(text, lang, name):
     global chosen_system, chosen_mode
-
     if chosen_system == 'vaja':
-        if chosen_mode == '1':
-            data = {'input_text': text, 'speaker': 0, 'phrase_break': 0, 'audiovisual': 0}
-        elif chosen_mode == '2':
-            data = {'input_text': text, 'speaker': 1, 'phrase_break': 0, 'audiovisual': 0}
-        elif chosen_mode == '3':
-            data = {'input_text': text, 'speaker': 2, 'phrase_break': 0, 'audiovisual': 0}
-        elif chosen_mode == '4':
-            data = {'input_text': text, 'speaker': 3, 'phrase_break': 0, 'audiovisual': 0}
-        else:
-            #print("Invalid mode for Vaja. Defaulting to mode 0.")
-            data = {'input_text': text, 'speaker': 0, 'phrase_break': 0, 'audiovisual': 0}
+        # simplified using a dictionary to map modes to speakers
+        speaker_mapping = {'1': 0, '2': 1, '3': 2, '4': 3}
+        speaker = speaker_mapping.get(chosen_mode, 0)
+        data = {'input_text': text, 'speaker': speaker, 'phrase_break': 0, 'audiovisual': 0}
         return synth_vaja(data, name)
     elif chosen_system == 'google':
         return google_tts(text, lang, name)
@@ -84,48 +101,33 @@ def generate_bard_response(text, token):
     cleaned_result = re.sub(r'\([^)]*\)|\*|\:', '', result)
     return cleaned_result
 
-def play(chosen_system,name):
-    if chosen_system =='vaja':
-        os.system(f'cvlc --play-and-exit {name}.wav')
-    elif chosen_system == 'google':
-        os.system(f'cvlc --play-and-exit {name}.mp3')   
-
-def transcribe_mic(msg):
+def transcribe_mic(secure_1psid_cookie):
     global chosen_system, chosen_mode
-    ask_tts_system('กรุณารอสักครู่', lan,'zwait')
-    play(chosen_system,'zwait')
-    ask_tts_system('มีอะไรถามเพิ่มเติมไหม', lan,'zagain')
-    ask_tts_system('สวัสดี ฉันเป็นผู้ช่วยด้านสุขภาพ อยากรู้อะไรถามได้เลยนะ', lan,'zrespond')
-    play(chosen_system,'zrespond')
+    ask_tts_system('กรุณารอสักครู่', lan, 'wait')
+    ask_tts_system('สวัสดี', lan, 'again')
+    play_sound(chosen_system,'wait')
+    play_sound(chosen_system,'again')    
     while True:
         try:
             transcribed_text = listen_and_transcribe()
             print(transcribed_text)
-            play(chosen_system,'zwait')
-            response = generate_bard_response(transcribed_text, msg)
+            play_sound(chosen_system, 'wait')
+            response = generate_bard_response(transcribed_text, secure_1psid_cookie)
             print(response)
-
-            ask_tts_system(response, lan ,'zrespond')
-            play(chosen_system,'zrespond')
-            play(chosen_system,'zagain')
-            # # At the end of the loop, ask if the user wants to change the TTS system or mode:
-            # change_system = input("Do you want to change the TTS system? (yes/no): ").lower()
-            # if change_system == 'yes':
-            #     chosen_system = input("Choose TTS system (vaja/google): ")
-            #     chosen_mode = None if chosen_system == "google" else input('Select mode in 0-4: ')
-
+            ask_tts_system(response, lan, 'zrespond')
+            play_sound(chosen_system,'zrespond')
         except sr.WaitTimeoutError:
             print("Recognition timed out")
         except AssertionError:
             print("No audio source available. Waiting for an audio source...")
         except requests.ConnectionError as e:
             print(f"Connection error occurred:{e}")
-            msg = None
+            secure_1psid_cookie = None
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
 def main():
-    transcribe_mic(msg)
+    transcribe_mic(secure_1psid_cookie)
 
 if __name__ == "__main__":
     main()
